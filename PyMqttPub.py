6b2554bd4bc6cc546ac5a4bff5edb5f0625604e9
ianagung <ian.agung@len.co.id>
2021-01-10 17:46:53 +0700

    Script for MQTT Pub Sub w/ username password

---
+++ PyMqttPub.py
@@ -0,0 +1,16 @@
+import paho.mqtt.client as mqtt
+
+broker_url = "localhost"
+broker_port = 1883
+
+client = mqtt.Client()
+
+# edit code for passwords
+print("setting  password")
+client1.username_pw_set(username="user01",password="mqtt")
+
+client.connect(broker_url, broker_port)
+
+client.publish(topic="sdn/fuzzyout", payload="TestingPayload", qos=1, retain=False)
+
+print("Do Something else")
---
+++ PyMqttSub.py
@@ -0,0 +1,39 @@
+import paho.mqtt.client as mqtt
+
+broker_url = "localhost"
+broker_port = 1883
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected With Result Code " (rc))
+    if rc==0:
+        print("connected OK Returned code=",rc)
+    else:
+        print("Bad connection Returned code=",rc)
+
+def on_disconnect(client, userdata, rc):
+    print("Client Got Disconnected")
+
+def on_message_from_fuzzyout(client, userdata, message):
+    print("Message Recieved from Fuzzy Module: "+message.payload.decode())
+
+def on_message(client, userdata, message):
+    print("Message Recieved from Others: "+message.payload.decode())
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+#To Process Every Other Message
+client.on_message = on_message
+# edit code for passwords
+print("setting  password")
+client1.username_pw_set(username="user01",password="mqtt")
+
+client.connect(broker_url, broker_port)
+
+client.subscribe("sdn/fuzzyout", qos=1)
+
+client.message_callback_add("sdn/fuzzyout", on_message_from_fuzzyout)
+
+client.loop_forever()
+
+print("Do Something else")
